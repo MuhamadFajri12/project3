ea80df01cd8b2504bf1b92fc624401a7c9ccab13
MuhamadFajri12 <mhmdfajri562@gmail.com>
2024-07-24 22:11:48 -0800

    project3

---
+++ app/Controllers/page.php
@@ -0,0 +1,16 @@
+<?php
+class Page extends BaseController
+{
+    public function about()
+    {
+        echo view("about");
+    }
+    public function contact()
+    {
+        echo view("contact");
+    }
+    public function faqs()
+    {
+        echo view("Faqs");
+    }
+}
